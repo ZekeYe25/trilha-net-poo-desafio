1c035dd88dd7007e2b17f7f771cf5e717d4a2206
Zekeye <trocodopao@hotmail.com>
2025-03-16 15:22:10 -0300

    Celular Atualizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphoneSe = new Iphone("123", "SE", "0-92", 520);
+
+iphoneSe.InstalarAplicativo("Whatssap");
+iphoneSe.Ligar();
+iphoneSe.ReceberLigacao();
